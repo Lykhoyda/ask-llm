7d1dba7db354c05df834cbfa1930a3396c9db023
Anton Lykhoyda <lykhoyda@gmail.com>
2026-07-14 15:45:43 +0200

    docs(diagrams): add animated RequestFlow and PairLoop SVG components

--- apps/docs/.vitepress/theme/index.ts
+++ apps/docs/.vitepress/theme/index.ts
@@ -5,6 +5,8 @@ import DiagramModal from "../components/DiagramModal.vue";
 import SetupTabs from "../components/SetupTabs.vue";
 import InAction from "../components/InAction.vue";
 import TroubleshootingModal from "../components/TroubleshootingModal.vue";
+import RequestFlow from "../components/diagrams/RequestFlow.vue";
+import PairLoop from "../components/diagrams/PairLoop.vue";
 import "./custom.css";
 
 export default {
@@ -15,5 +17,7 @@ export default {
     app.component("SetupTabs", SetupTabs);
     app.component("InAction", InAction);
     app.component("TroubleshootingModal", TroubleshootingModal);
+    app.component("RequestFlow", RequestFlow);
+    app.component("PairLoop", PairLoop);
   },
 } satisfies Theme;
